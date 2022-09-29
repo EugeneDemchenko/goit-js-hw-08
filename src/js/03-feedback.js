91b5ee0271efbb28cebd42d1b72ac10b0d3f32b1
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-09-29 11:34:51 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,13 @@
 import throttle from 'lodash.throttle'
 
-const formData = {};
+
 
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
     email: document.querySelector('.feedback-form input'),
 }
+
 
 populateTextArea();
 refs.form.addEventListener('submit', onFormSubmit);
@@ -15,14 +16,18 @@ refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 function onFormSubmit(event) {
     event.preventDefault();
-    if(refs.email.value === '' && refs.textarea.value === '') { return } else {console.log({ email: refs.email.value, textarea: refs.textarea.value });}
+    if (refs.email.value === '' && refs.textarea.value === '') { return }
+    else { console.log({ email: refs.email.value, message: refs.textarea.value }); }
     
     event.target.reset();
     localStorage.removeItem("feedback-form-state");
 } 
 
-function onTextareaInput(event) {
-    // const text = event.target.value;
+function onTextareaInput() {
+    const formData = {
+    email: refs.email.value,
+    message: refs.textarea.value,
+};
     localStorage.setItem("feedback-form-state", JSON.stringify(formData))
 }
 
